7becbd89366ce7f37d1e35f03355c4412357cbaa
Marcellyz <marcelly.eduarda4545@gmail.com>
2025-01-07 23:54:03 -0300

    feat(new): Criar pagina Editar Cliente igual a pagina do cadastrar Cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import { ListarClientesComponent } from './pages/listar-clientes/listar-clientes.component';
 import { Routes } from '@angular/router';
+
+import { ListarClientesComponent } from './pages/listar-clientes/listar-clientes.component';
 import { CadastrarClientesComponent } from './pages/cadastrar-clientes/cadastrar-clientes.component';
-import { HomeComponent } from './pages/home/home.component';
+import { EditarClientesComponent } from './pages/editar-clientes/editar-clientes.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'listar-clientes', pathMatch: 'full' },
   { path: 'listar-clientes', component: ListarClientesComponent },
   { path: 'cadastrar-novo-cliente', component:  CadastrarClientesComponent },
+  { path: 'editar-clientes', component: EditarClientesComponent },
 ];
---
+++ src/app/pages/editar-clientes/editar-clientes.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { RouterLink } from '@angular/router';
+
+import { Cliente } from '../../model/Cliente';
+import { ClienteService } from '../../service/cliente.service';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-editar-clientes',
+  imports: [
+    HeaderComponent,
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './editar-clientes.component.html',
+  styleUrl: './editar-clientes.component.scss',
+})
+export class EditarClientesComponent {
+  clienteForm: FormGroup; // Declarando o FormGroup para o formulário
+
+  constructor(private fb: FormBuilder, private clienteService: ClienteService) {
+    // Inicializando o FormGroup com campos e validações
+    this.clienteForm = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      idade: ['', [Validators.required, Validators.min(1)]],
+      cidade: ['', [Validators.required, Validators.minLength(3)]],
+    });
+  }
+
+  editarCliente() {
+    if (this.clienteForm.valid) {
+      const cliente: Cliente = this.clienteForm.value;
+      this.clienteService.editarCliente(cliente).subscribe({
+        next: () => {
+          alert('Cliente cadastrado com sucesso!');
+          this.clienteForm.reset();
+        },
+        error: (error) => {
+          console.error('Erro ao cadastrar cliente:', error);
+        },
+      });
+    } else {
+      alert('Por favor, preencha o formulário corretamente.');
+    }
+  }
+}
